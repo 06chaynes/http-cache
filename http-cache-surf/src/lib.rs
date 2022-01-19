e52e3e92f879e78ca8a749f4af38851257e123e8
Christian Haynes <06chaynes@gmail.com>
2022-01-19 14:01:13 -0500

    added tests for NoCache and ForceCache modes

--- http-cache-surf/src/lib.rs
+++ http-cache-surf/src/lib.rs
@@ -246,6 +246,75 @@ mod tests {
         Ok(())
     }
 
+    #[async_std::test]
+    async fn no_cache_mode() -> surf::Result<()> {
+        let m = build_mock_server("max-age=86400, public", TEST_BODY, 200, 2);
+        let url = format!("{}/", &mockito::server_url());
+        let manager = CACacheManager::default();
+        let path = manager.path.clone();
+        let key = format!("{}:{}", GET, &url);
+        let req = Request::new(Method::Get, Url::parse(&url)?);
+
+        // Make sure the record doesn't already exist
+        manager.delete(GET, &Url::parse(&url)?).await?;
+
+        // Construct Surf client with cache defaults
+        let client = Client::new().with(Cache(HttpCache {
+            mode: CacheMode::NoCache,
+            manager: CACacheManager::default(),
+            options: None,
+        }));
+
+        // Remote request and should cache
+        client.send(req.clone()).await?;
+
+        // Try to load cached object
+        let data = cacache::read(&path, &key).await;
+        assert!(data.is_ok());
+
+        // To verify our endpoint receives the request rather than a cache hit
+        client.send(req.clone()).await?;
+        m.assert();
+        manager.clear().await?;
+        Ok(())
+    }
+
+    #[async_std::test]
+    async fn force_cache_mode() -> surf::Result<()> {
+        let m =
+            build_mock_server("max-age=86400, public", TEST_BODY, 200, 1);
+        let url = format!("{}/", &mockito::server_url());
+        let manager = CACacheManager::default();
+        let path = manager.path.clone();
+        let key = format!("{}:{}", GET, &url);
+        let req = Request::new(Method::Get, Url::parse(&url)?);
+
+        // Make sure the record doesn't already exist
+        manager.delete(GET, &Url::parse(&url)?).await?;
+
+        // Construct Surf client with cache defaults
+        let client = Client::new().with(Cache(HttpCache {
+            mode: CacheMode::ForceCache,
+            manager: CACacheManager::default(),
+            options: None,
+        }));
+
+        // Should result in a cache miss and a remote request
+        client.send(req.clone()).await?;
+
+        // Try to load cached object
+        let data = cacache::read(&path, &key).await;
+        assert!(data.is_ok());
+
+        // Should result in a cache hit and no remote request
+        client.send(req.clone()).await?;
+
+        // Verify endpoint did receive the request
+        m.assert();
+        manager.clear().await?;
+        Ok(())
+    }
+
     #[cfg(test)]
     mod only_if_cached_mode {
         use super::*;
